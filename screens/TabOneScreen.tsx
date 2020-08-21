fda890ed405f4b9adaa78c07ee54bfcd1fdc3973
Brigi <gaspar.brigitta867@gmail.com>
2020-08-21 22:24:24 +0200

    i18n added

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,42 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Switch, View } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text } from '../components/Themed';
+import { Card, Button, Icon } from 'react-native-elements';
+import { useTranslation } from 'react-i18next';
+// @ts-ignore
+import i18n from '../i18n';
+import i18next from 'i18next';
+
+const changeLanguage = () => {
+  i18next.language === 'de' ? i18n.changeLanguage('en') : i18n.changeLanguage('de');
+};
 
 export default function TabOneScreen() {
+  const {t} = useTranslation();
+  const isEnabled = i18next.language === 'de';
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <Card
+      title={<View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <Text style={{color: '#555555'}}>{t('hello')}</Text>
+        <Switch
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={changeLanguage}
+          value={isEnabled}
+        />
+      </View>}
+      image={require('../assets/images/logan-weaver-u76Gd0hP5w4-unsplash.jpg')}>
+      <Text style={{marginBottom: 10, color: '#555555'}}>
+        {t('details')}
+      </Text>
+      <Button
+        icon={<Icon name='code' color='#ffffff' />}
+        buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+        title={t('button')} />
+    </Card>
   );
 }
 
